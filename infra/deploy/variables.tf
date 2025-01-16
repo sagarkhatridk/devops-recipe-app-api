32a72a74c2677f555dbbb143f2c46ecf43bdb8cc
sagarkhatridk <sagarkhatridk@gmail.com>
2025-01-16 23:37:08 +0530

    Added terraform

---
+++ infra/deploy/variables.tf
@@ -0,0 +1,15 @@
+variable "prefix" {
+  description = "Prefix for all resources"
+  default     = "raa"
+}
+
+variable "project" {
+  description = "Project name for tagging resources"
+  default     = "recipe-app-api"
+}
+
+variable "contact" {
+  description = "Contact email for tagging resources"
+  default     = "sagarkhatridk@gmail.com"
+}
+
